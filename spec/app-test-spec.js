c143f5fcd1dbd6971cb283dcd13415729815fa28
Lucas <rukaso.mero@gmail.com>
2017-11-18 00:21:03 -0200

    aspas simples

--- spec/app-test-spec.js
+++ spec/app-test-spec.js
@@ -1,12 +1,12 @@
-var request = require("request");
+var request = require('request');
 
-var base_url = "http://localhost:5000"
+var base_url = 'http://localhost:5000'
 
-describe("Teste de App", function() {
+describe('Teste de App', function() {
 
-        describe("GET /conta", function() {
+        describe('GET /conta', function() {
             
-            it("returns status code 200", function() {
+            it('returns status code 200', function() {
                 request.get(base_url + '/conta', function(error, response, body) {
                 expect(response.statusCode).toBe(200);
                 });
